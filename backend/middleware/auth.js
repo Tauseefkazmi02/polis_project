4bedba74606e4b63895e237b57cb6c7340a1e77f
Tauseefkazmi02 <syedsaifalikazmi@gmail.com>
2025-08-17 19:42:15 +0530

    Create auth.js

---
+++ backend/middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const secretKey = 'your_secret_key'; // Replace with env variable in production
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+function authorizeRoles(...roles) {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ error: 'Access denied' });
+    }
+    next();
+  };
+}
+
+module.exports = { authenticateToken, authorizeRoles, secretKey };
